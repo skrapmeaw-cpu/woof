7c4e35edc3b8eb7fc2b0dc89a560e1cf7948db1b
skrapmeaw-cpu <skrapmeaw@gmail.com>
2026-04-23 12:35:22 +0200

    Uppdatera pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 basic.forever(function () {
-    basic.showLeds(`
-        # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
-        # . . . #
-        `)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Asleep)
+    basic.showIcon(IconNames.Confused)
+    basic.showIcon(IconNames.Sad)
 })
